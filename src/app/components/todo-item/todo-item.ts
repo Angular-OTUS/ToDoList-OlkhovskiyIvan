c618a924fcc06f92be4043c3f8bca77454c14d49
vanst <vanster@mail.ru>
2026-01-31 20:21:24 +0500

    РНО ДЗ 2

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,18 +1,17 @@
-import { ChangeDetectionStrategy, Component, input, Input, InputSignal, output, Signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal, output } from '@angular/core';
 import { ConstImgValue } from '../../models/constants';
 
 @Component({
   selector: 'app-todo-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss',
 })
 export class TodoItem {
 
-  public img = ConstImgValue;
+  protected  img = ConstImgValue;
   public taskName: InputSignal<string> = input.required<string>();
-  public delTask = output();
+  protected delTask = output();
 
   onClickDell() {
     this.delTask.emit();
