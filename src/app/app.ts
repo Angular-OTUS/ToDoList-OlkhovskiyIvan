33625a7cc26367c6922c0762022abc14a9b2bc99
vanst <vanster@mail.ru>
2026-03-31 11:24:51 +0500

    ДЗ-7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { ToDoList } from "./components/to-do-list/to-do-list";
 import { ImgPath } from './models/constants';
 import { ToastsComponent } from "./components/toasts-component/toasts-component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [ToDoList, ToastsComponent],
+  imports: [ToastsComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
